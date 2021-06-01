b1b5ac2498ee9e2002478a97241ef76301fc0cb7
dom <199607321@163.com>
2021-06-01 21:49:28 +0800

    initial version of test springboot

---
+++ src/main/java/com/example/demo/test/SpringbootTestApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class SpringbootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestApplication.class, args);
+	}
+
+
+}
